385ead0f80e53e06fc6186dde98385beb33519eb
slaninas <uglymuffin@protonmail.com>
2020-10-13 21:44:26 +0200

    Add more addresses, move cartridge info printing into Cartridge class

--- cartridge.h
+++ cartridge.h
@@ -22,6 +22,7 @@ auto convert(const TInput& source, TOutput& destination) {
 	);
 }
 
+// TODO: Add check for header checksum?
 class Cartridge {
 public:
 	Cartridge(const std::string& filename) {
@@ -35,7 +36,12 @@ public:
 	static inline std::map<const char*, std::pair<std::uint16_t, std::uint16_t>> addreses = {
 		{"nintendo_logo",     {0x104, 0x134}},
 		{"title",             {0x134, 0x13f}},
-		{"manufacturer_code", {0x13f, 0x143}}
+		{"manufacturer_code", {0x13f, 0x143}},
+		{"cbg_flag",          {0x143, 0x144}},
+		{"rom_size_code",     {0x148, 0x149}},
+		{"ram_size_code",     {0x149, 0x14a}},
+		{"destination_code",  {0x14a, 0x14b}},
+		{"header_checksum",   {0x14d, 0x14e}}
 	};
 
 	// TODO: Move these print* methods elsewhere?
@@ -46,6 +52,7 @@ public:
 		std::cout << '\n';
 	}
 
+	// TODO: Delete all (start, end) methods, leave just (std::pair) versions
 	void print_as_hex(const uint16_t start, const uint16_t end) {
 		std::cout << std::hex;
 		for (auto i = start; i < end; ++i) {
@@ -53,6 +60,11 @@ public:
 		}
 		std::cout << '\n';
 		std::cout << std::dec;
+	}
+
+	auto print_as_hex(const std::pair<uint16_t, uint16_t>& range) {
+		const auto [start, end] = range;
+		return print_as_hex(start, end);
 	}
 
 	void print_as_int(const uint16_t start, const uint16_t end) {
@@ -68,6 +80,11 @@ public:
 		auto bytes_as_string = std::string{};
 		convert(bytes, bytes_as_string);
 		return bytes_as_string;
+	}
+
+	auto get_as_string(const std::pair<uint16_t, uint16_t>& range) {
+		const auto [start, end] = range;
+		return get_as_string(start, end);
 	}
 
 	auto get_title() {
@@ -101,6 +118,24 @@ public:
 		}
 		return static_cast<int>(sum);
 	}
+
+	void print_info() {
+		std::cout << "Cartridge Info:\n";
+		std::cout << std::string(20, '-') << '\n';
+
+		std::cout << "Title: " << get_as_string(addreses["title"]) << '\n';
+		std::cout << "Manufacturer Code: " << get_as_string(addreses["manufacturer_code"]) << '\n';
+		std::cout << "CBG Flag: "; print_as_hex(addreses["cbg_flag"]);
+		std::cout << "ROM Size Code: "; print_as_hex(addreses["rom_size_code"]);
+		std::cout << "RAM Size Code (in cartridge): "; print_as_hex(addreses["ram_size_code"]);
+		std::cout << "Destination Code: "; print_as_hex(addreses["destination_code"]);
+		std::cout << "Header Checksum: "; print_as_hex(addreses["header_checksum"]);
+
+		std::cout << std::hex;
+		std::cout << "Calculated Header Checksum: " << get_header_checksum() << '\n';
+		std::cout << std::dec;
+	}
+
 
 private:
 	std::vector<std::byte> buffer_;
--- main.cc
+++ main.cc
@@ -25,27 +25,8 @@ int main(int argc, const char** argv) {
 	auto filename = std::string(argv[1]);
 
 	auto cart = Cartridge{filename};
-	std::cout << "Title: " << cart.get_title() << '\n';
-	std::cout << "Manufacturer Code: " << cart.get_manufacturer_code() << '\n';
+	cart.print_info();
 	// cart.print_hex_logo();
 
-	std::cout << "CGB Flag: ";
-	cart.print_as_hex(0x143, 0x143 + 1);
-
-	std::cout << "ROM Size Code: ";
-	cart.print_as_hex(0x148, 0x148 + 1);
-
-	std::cout << "RAM Size Code (in cartridge): ";
-	cart.print_as_hex(0x149, 0x149 + 1);
-
-	std::cout << "Destination Code: ";
-	cart.print_as_hex(0x14A, 0x14A + 1);
-
-	std::cout << "Header Checksum: ";
-	cart.print_as_hex(0x14D, 0x14D + 1);
-
-	std::cout << std::hex;
-	std::cout << "Calculated Header Checksum: " << cart.get_header_checksum() << '\n';
-	std::cout << std::dec;
 
 }

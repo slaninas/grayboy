9b7e9f4cb87989b35a4dc706ecccb456b2ba4b5d
slaninas <uglymuffin@protonmail.com>
2020-10-16 20:45:32 +0200

    Rename test case, add check

--- tests/cpu_ld_tests.cc
+++ tests/cpu_ld_tests.cc
@@ -4,8 +4,12 @@
 #include "cpu.h"
 
 
-TEST_CASE("LD BC, d16", "[ld]") {
-	auto memory = Cpu::MemoryType{0x01, 0xEE, 0xFF, 0x01, 0xAB, 0xCD};
+TEST_CASE("LD BC, d16 - 0x01", "[ld]") {
+	auto memory = Cpu::MemoryType{
+		0x01, 0xEE, 0xFF,
+		0x01, 0xAB, 0xCD,
+		0x01, 0x01, 0x00
+	};
 
 	auto cpu = Cpu{std::move(memory)};
 
@@ -29,10 +33,14 @@ TEST_CASE("LD BC, d16", "[ld]") {
 	correct_state.PC = 0x06;
 	CHECK_THAT(cpu.get_registers_dump(), RegistersCompare{correct_state});
 
+	const auto cycles3 = cpu.execute_next();
+	correct_state.clear();
+	correct_state.B = 0x01;
+	correct_state.C = 0x00;
+	correct_state.PC = 0x09;
+	CHECK_THAT(cpu.get_registers_dump(), RegistersCompare{correct_state});
+
 	cpu.clear_registers();
 	// TODO: Check this in tests for registers
 	CHECK_THAT(cpu.get_registers_dump(), RegistersCompare{empty_regs});
-
-
-
 }

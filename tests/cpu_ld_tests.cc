3b659ec7f2fada2fa960e27e516b4a080edbce71
slaninas <uglymuffin@protonmail.com>
2020-10-17 17:40:17 +0200

    Cleanup + use SECTION in test

--- tests/cpu_ld_tests.cc
+++ tests/cpu_ld_tests.cc
@@ -19,24 +19,26 @@ TEST_CASE("LD BC, d16 - 0x01", "[ld]") {
 	SECTION("Copy 0xEEFF into BC") {
 		const auto cycles = cpu.execute_next();
 		CHECK(cycles == 3);
-		auto correct_state = MakeRegisters{.B=0xEE, .C=0xFF, .PC=0x03}.get();
+
+		const auto correct_state = MakeRegisters{.B=0xEE, .C=0xFF, .PC=0x03}.get();
 		CHECK_THAT(cpu.registers_dump(), RegistersCompare{correct_state});
 	}
 
 	SECTION("Copy 0xABCD into BC") {
 		cpu.registers().write_PC(0x03);
-		const auto cycles2 = cpu.execute_next();
-		CHECK(cycles2 == 3);
-		auto correct_state = MakeRegisters{.B=0xAB, .C=0xCD, .PC=0x06}.get();
+		const auto cycles = cpu.execute_next();
+		CHECK(cycles == 3);
+
+		const auto correct_state = MakeRegisters{.B=0xAB, .C=0xCD, .PC=0x06}.get();
 		CHECK_THAT(cpu.registers_dump(), RegistersCompare{correct_state});
 	}
 
 	SECTION("Copy 0x0100 into BC") {
 		cpu.registers().write_PC(0x06);
-		const auto cycles3 = cpu.execute_next();
-		CHECK(cycles3 == 3);
+		const auto cycles = cpu.execute_next();
+		CHECK(cycles == 3);
 
-		auto correct_state = MakeRegisters{.B=0x01, .C=0x00, .PC=0x09}.get();
+		const auto correct_state = MakeRegisters{.B=0x01, .C=0x00, .PC=0x09}.get();
 		CHECK_THAT(cpu.registers_dump(), RegistersCompare{correct_state});
 	}
 
@@ -50,19 +52,18 @@ TEST_CASE("LD (BC), A - 0x02", "[ld]") {
 	auto memory = Cpu::MemoryType{0x02};
 
 	auto cpu = Cpu{std::move(memory)};
-	cpu.registers().write_A(0x12);
-	cpu.registers().write_BC(0x00);
+	SECTION("Copy 0x12 from A into (BC) (0x00)") {
+		cpu.registers().write_A(0x12);
+		cpu.registers().write_BC(0x00);
 
-	auto register_before = cpu.registers();
-	register_before.write_PC(register_before.read_PC() + 1);
+		const auto cycles = cpu.execute_next();
+		CHECK(cycles == 2);
 
-	const auto cycles = cpu.execute_next();
-	CHECK(cycles == 2);
+		const auto correct_memory = Cpu::MemoryType{0x12};
+		CHECK(cpu.memory_dump() == correct_memory);
 
-	auto correct_memory = Cpu::MemoryType{0x12};
-	CHECK(cpu.memory_dump() == correct_memory);
-
-	auto correct_registers = MakeRegisters{.A=0x12, .BC=0x00, .PC=0x01}.get();
-	CHECK_THAT(cpu.registers(), RegistersCompare{correct_registers});
+		const auto correct_registers = MakeRegisters{.A=0x12, .BC=0x00, .PC=0x01}.get();
+		CHECK_THAT(cpu.registers(), RegistersCompare{correct_registers});
+	}
 
 }

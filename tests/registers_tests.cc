40bc00894538f36398e4fb6c4e2fb18ae9ee9cac
slaninas <uglymuffin@protonmail.com>
2020-10-17 09:26:04 +0200

    Add skeleton for registers tests

---
+++ tests/registers_tests.cc
@@ -0,0 +1,9 @@
+#include "catch2/catch.hpp"
+
+#include <stdexcept>
+
+TEST_CASE("Registers initialization", "[registers]") {
+	throw std::logic_error("Not implemented yet.");
+}
+
+// TODO: Add more

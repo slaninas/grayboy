993c08e2a8cb4912b7011af50bcd8384ab119efb
slaninas <uglymuffin@protonmail.com>
2020-10-18 20:56:59 +0200

    Improve arithmetic tests

--- tests/cpu_arithmetic_tests.cc
+++ tests/cpu_arithmetic_tests.cc
@@ -26,24 +26,26 @@ TEST_CASE("INC BC - 0x03", "[arithmetic]") {
 
 TEST_CASE("INC B - 0x04", "[arithmetic]") {
 	auto cpu = Cpu{{0x04, 0x04, 0x04}};
-	cpu.registers().write("F", 0xff);
+	const auto F_init = 0xff;
+	const auto F_low_nibbel = F_init & 0x0f;
+	cpu.registers().write("F", F_init);
 
 	SECTION("From zero to three") {
 		auto cycles = cpu.execute_next();
 		CHECK(cycles == 1);
-		auto correct_F = MakeFlags{.Z=0, .N=0, .H=0, .C=1}.get() + 0xf; // Second half of byte should stay unchanged thus + 0xf
+		auto correct_F = MakeFlags{.Z=0, .N=0, .H=0, .C=1}.get() + F_low_nibbel;
 		auto correct_registers = MakeRegisters{.F=correct_F, .B=0x01, .PC=0x01}.get();
 		CHECK_THAT(cpu.registers(), RegistersCompare{correct_registers});
 
 		cycles = cpu.execute_next();
 		CHECK(cycles == 1);
-		correct_F = MakeFlags{.Z=0, .N=0, .H=0, .C=1}.get() + 0xf;
+		correct_F = MakeFlags{.Z=0, .N=0, .H=0, .C=1}.get() + F_low_nibbel;
 		correct_registers = MakeRegisters{.F=correct_F, .B=0x02, .PC=0x02}.get();
 		CHECK_THAT(cpu.registers(), RegistersCompare{correct_registers});
 
 		cycles = cpu.execute_next();
 		CHECK(cycles == 1);
-		correct_F = MakeFlags{.Z=0, .N=0, .H=0, .C=1}.get() + 0xf;
+		correct_F = MakeFlags{.Z=0, .N=0, .H=0, .C=1}.get() + F_low_nibbel;
 		correct_registers = MakeRegisters{.F=correct_F, .B=0x03, .PC=0x03}.get();
 		CHECK_THAT(cpu.registers(), RegistersCompare{correct_registers});
 	}
@@ -53,7 +55,7 @@ TEST_CASE("INC B - 0x04", "[arithmetic]") {
 		auto cycles = cpu.execute_next();
 		CHECK(cycles == 1);
 
-		const auto correct_F = MakeFlags{.Z=1, .N=0, .H=1, .C=1}.get() + 0xf;
+		const auto correct_F = MakeFlags{.Z=1, .N=0, .H=1, .C=1}.get() + F_low_nibbel;
 		const auto correct_registers = MakeRegisters{.F=correct_F, .B=0x00, .PC=0x01}.get();
 		CHECK_THAT(cpu.registers(), RegistersCompare{correct_registers});
 	}
@@ -63,13 +65,13 @@ TEST_CASE("INC B - 0x04", "[arithmetic]") {
 		auto cycles = cpu.execute_next();
 		CHECK(cycles == 1);
 
-		auto correct_F = MakeFlags{.Z=0, .N=0, .H=1, .C=1}.get() + 0xf;
+		auto correct_F = MakeFlags{.Z=0, .N=0, .H=1, .C=1}.get() + F_low_nibbel;
 		auto correct_registers = MakeRegisters{.F=correct_F, .B=0x10, .PC=0x01}.get();
 		CHECK_THAT(cpu.registers(), RegistersCompare{correct_registers});
 
 		cycles = cpu.execute_next();
 		CHECK(cycles == 1);
-		correct_F = MakeFlags{.Z=0, .N=0, .H=0, .C=1}.get() + 0xf;
+		correct_F = MakeFlags{.Z=0, .N=0, .H=0, .C=1}.get() + F_low_nibbel;
 		correct_registers = MakeRegisters{.F=correct_F, .B=0x11, .PC=0x02}.get();
 		CHECK_THAT(cpu.registers(), RegistersCompare{correct_registers});
 	}

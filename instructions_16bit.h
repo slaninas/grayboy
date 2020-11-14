8fcf4a1a2a20f62e96b52ffa1a5ac2dfa9a0ec96
slaninas <uglymuffin@protonmail.com>
2020-11-14 10:42:24 +0100

    Add RRC *

--- instructions_16bit.h
+++ instructions_16bit.h
@@ -57,5 +57,60 @@ auto get_16bit_instructions() {
 				return 2;
 			}
 		},
+
+		{"RRC B", 0xcb08, 2,
+			[](auto& regs, [[maybe_unused]] auto& memory, [[maybe_unused]] const auto& PC) {
+				instruction_rrc("A", regs);
+				return 2;
+			}
+		},
+		{"RRC C", 0xcb09, 2,
+			[](auto& regs, [[maybe_unused]] auto& memory, [[maybe_unused]] const auto& PC) {
+				instruction_rrc("C", regs);
+				return 2;
+			}
+		},
+		{"RRC D", 0xcb0a, 2,
+			[](auto& regs, [[maybe_unused]] auto& memory, [[maybe_unused]] const auto& PC) {
+				instruction_rrc("D", regs);
+				return 2;
+			}
+		},
+		{"RRC E", 0xcb0b, 2,
+			[](auto& regs, [[maybe_unused]] auto& memory, [[maybe_unused]] const auto& PC) {
+				instruction_rrc("E", regs);
+				return 2;
+			}
+		},
+		{"RRC H", 0xcb0c, 2,
+			[](auto& regs, [[maybe_unused]] auto& memory, [[maybe_unused]] const auto& PC) {
+				instruction_rrc("H", regs);
+				return 2;
+			}
+		},
+		{"RRC L", 0xcb0d, 2,
+			[](auto& regs, [[maybe_unused]] auto& memory, [[maybe_unused]] const auto& PC) {
+				instruction_rrc("R", regs);
+				return 2;
+			}
+		},
+		{"RRC (HL)", 0xcb0e, 2,
+			[](auto& regs, auto& memory, const auto& PC) {
+				const auto address = memory.read(PC + 1);
+				const auto value = memory.read(address);
+				const auto [new_value, carry] = rrc(value);
+
+				memory.write(address, new_value);
+				set_flags_for_rotate(regs, new_value, carry);
+				return 4;
+			}
+		},
+		{"RRC A", 0xcb0f, 2,
+			[](auto& regs, [[maybe_unused]] auto& memory, [[maybe_unused]] const auto& PC) {
+				instruction_rrc("A", regs);
+				return 2;
+			}
+		},
+
 	};
 }

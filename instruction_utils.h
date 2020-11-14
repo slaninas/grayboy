25d6434688334d738bfc403e4f4212a8fbdbd22e
slaninas <uglymuffin@protonmail.com>
2020-11-14 10:27:21 +0100

    Add functions for RRC instructions

--- instruction_utils.h
+++ instruction_utils.h
@@ -339,3 +339,20 @@ void instruction_rl(const char (&reg_name)[2], Registers& regs) {
 	regs.set_flag("H", 0);
 	regs.set_flag("C", carry);
 }
+
+auto rrc(uint8_t old_value) {
+	const auto carry = static_cast<bool>(old_value & 1);
+	const auto new_value = static_cast<uint8_t>((old_value >> 1) + (carry << 7));
+	return std::pair{new_value, carry};
+}
+
+void instruction_rrc(const char (&reg_name)[2], Registers& regs) {
+	const auto old_value = regs.read(reg_name);
+	const auto [new_value, carry] = rrc(old_value);
+
+	regs.write(reg_name, new_value);
+	regs.set_flag("Z", new_value == 0);
+	regs.set_flag("N", 0);
+	regs.set_flag("H", 0);
+	regs.set_flag("C", carry);
+}
--- tests/cpu_16bit_instruction_tests.cc
+++ tests/cpu_16bit_instruction_tests.cc
@@ -30,3 +30,14 @@ TEST_CASE("RL", "[bit_operations]") {
 	CHECK_THAT(regs, RegistersCompare{correct_regs});
 }
 
+TEST_CASE("RRC", "[bit_operations]") {
+	const auto orig_flags = FlagsChanger{.C=0}.get(getRandomFlags());
+	const auto orig_regs = RegistersChanger{.F=orig_flags, .B=0xe9}.get(getRandomRegisters());
+
+	auto regs = orig_regs;
+	instruction_rrc("B", regs);
+
+	const auto correct_flags = FlagsChanger{.Z=0, .N=0, .H=0, .C=1}.get(orig_flags);
+	const auto correct_regs = RegistersChanger{.F=correct_flags, .B=0xf4}.get(orig_regs);
+	CHECK_THAT(regs, RegistersCompare{correct_regs});
+}

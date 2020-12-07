cb2a2d3c1c9248b104a7a01dcdd4a14cac9c79e7
slaninas <uglymuffin@protonmail.com>
2020-12-07 19:19:59 +0100

    Add function that checks which 8bit instructions are implemented

--- instructions.h
+++ instructions.h
@@ -14,3 +14,45 @@ auto get_all_instructions() {
 
 	return instructions_8bit;
 }
+
+
+void check_implemented() {
+	std::cout << std::hex;
+	auto omitted_8bit_opcodes = std::vector<uint8_t>{
+		0xcb,
+		0xd3,
+		0xdb,
+		0xdd,
+		0xe3,
+		0xe4,
+		0xeb,
+		0xec,
+		0xed,
+		0xf4,
+		0xfc,
+		0xfd
+	};
+
+	// Check 8bit opcodes
+	auto all_8bit = get_8bit_instructions();
+	for(uint8_t opcode = 0x00; opcode <= 0xff; ++opcode) {
+		auto is_implemented = std::find_if(cbegin(all_8bit), cend(all_8bit), [opcode](const auto& instruction) {	return instruction.opcode == opcode; }) != cend(all_8bit);
+
+		auto should_be_implemented = std::find_if(
+			cbegin(omitted_8bit_opcodes),
+			cend(omitted_8bit_opcodes),
+			[opcode](const auto& instruction_opcode) {	return instruction_opcode == opcode; }
+		) == cend(omitted_8bit_opcodes);
+
+		if (should_be_implemented && !is_implemented) {
+			std::cout << "Instruction 0x" << (int)opcode << " not found among implemented 8bit instructions.\n";
+		}
+		else if (!should_be_implemented && is_implemented) {
+			std::cout << "Instruction 0x" << (int)opcode << " is implemented but should not be.\n";
+		}
+	}
+
+	// TODO: Check 0xcb* opcodes
+
+	std::cout << std::dec;
+}

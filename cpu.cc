b0d71f3371a9ea2f855009ace4b7c581282162b5
slaninas <uglymuffin@protonmail.com>
2021-02-23 20:51:32 +0100

    Return by value

--- cpu.cc
+++ cpu.cc
@@ -2,7 +2,7 @@
 
 #include <optional>
 
-[[nodiscard]] const Instruction& find_by_opcode(const uint16_t opcode, const std::vector<Instruction>& instructions) {
+[[nodiscard]] const Instruction find_by_opcode(const uint16_t opcode, const std::vector<Instruction>& instructions) {
 	auto res = std::find_if(begin(instructions), end(instructions), [opcode](const auto& instruction) { return instruction.opcode ==  opcode; });
 	if (res == end(instructions)) {
 		auto stream = std::stringstream{};

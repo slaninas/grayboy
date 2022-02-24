37e41000af5b30822aeaf7e383749b6a92a0993c
slaninas <uglymuffin@protonmail.com>
2022-02-24 13:38:21 +0100

    Add logging

--- src/cpu.h
+++ src/cpu.h
@@ -20,6 +20,10 @@ struct DisassemblyInfo {
 
 [[nodiscard]] auto find_by_opcode(const uint16_t opcode, const std::vector<Instruction>& instructions) -> Instruction;
 
+inline std::ofstream debug_log("debug_log");
+class Cpu;
+inline void log(const Cpu& cpu);
+
 // Make exceptions asserts and run in debug
 // TODO: Add unit tests
 class Cpu {
@@ -49,6 +53,7 @@ public:
 
 	[[nodiscard]] auto execute_next()
 	{
+		log(*this);
 		const auto PC = regs_.read("PC");
 		const auto opcode = get_opcode(PC);
 
@@ -92,6 +97,11 @@ public:
 		return regs_;
 	}
 
+	[[nodiscard]] auto registers() const  -> const auto&
+	{
+		return regs_;
+	}
+
 	// TODO: Remove this once I can do snapshots of whole cpu state
 	[[nodiscard]] auto get_memory() const {
 		return memory_;
@@ -108,4 +118,45 @@ private:
 	{
 		return ::find_by_opcode(opcode, instructions_);
 	}
+
 };
+
+
+inline auto get_formatted(const int& value, const uint32_t& width) -> std::string {
+	auto s = std::stringstream{};
+	s << std::setfill('0') << std::setw(width) << std::hex << value;
+	auto result = s.str();
+	std::for_each(begin(result), end(result), [](auto& c) { c = std::toupper(c); });
+
+
+	return result;
+}
+
+void log(const Cpu& cpu) {
+
+	debug_log << std::hex;
+	debug_log << "A: " << get_formatted(cpu.registers().read("A"), 2) << ' ';
+	debug_log << "F: " << get_formatted(cpu.registers().read("F"), 2) << ' ';
+	debug_log << "B: " << get_formatted(cpu.registers().read("B"), 2) << ' ';
+	debug_log << "C: " << get_formatted(cpu.registers().read("C"), 2) << ' ';
+	debug_log << "D: " << get_formatted(cpu.registers().read("D"), 2) << ' ';
+	debug_log << "E: " << get_formatted(cpu.registers().read("E"), 2) << ' ';
+	debug_log << "H: " << get_formatted(cpu.registers().read("H"), 2) << ' ';
+	debug_log << "L: " << get_formatted(cpu.registers().read("L"), 2) << ' ';
+
+	debug_log << "SP: " << get_formatted(cpu.registers().read("SP"), 4) << ' ';
+
+	const auto PC = cpu.registers().read("PC");
+	// TODO: Don't hardcode
+	debug_log << "PC: 00:" << get_formatted(PC, 4) << ' ';
+	debug_log << '(';
+	debug_log << std::hex << get_formatted(cpu.get_memory().read(PC), 2) << ' ';
+	debug_log << std::hex << get_formatted(cpu.get_memory().read(PC + 1), 2) << ' ';
+	debug_log << std::hex << get_formatted(cpu.get_memory().read(PC + 2), 2) << ' ';
+	debug_log << std::hex << get_formatted(cpu.get_memory().read(PC + 3), 2);
+	debug_log << ')';
+	debug_log << std::endl;;
+
+
+}
+

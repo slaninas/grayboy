2dd5a8ae2424472d022a828f5dd44dde5bef23b1
slaninas <uglymuffin@protonmail.com>
2022-04-11 10:44:56 +0200

    Add Emulator class

---
+++ src/emulator.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "cartridge.h"
+#include "cpu.h"
+
+class Emulator {
+public:
+	Emulator(const std::string& cartridge_path) {
+
+		const auto rom = Cartridge{cartridge_path}.dump();
+
+		auto array = Memory::ArrayType{};
+
+		std::transform(begin(rom), end(rom), begin(array), [](const auto& el) { return static_cast<uint8_t>(el); });
+
+		// TODO: Remove, for debug purposes only
+		array[0xff44] = 0x90;
+
+		const auto regs =
+			Registers{};
+
+		cpu_ = Cpu{array, regs};
+		raw_dump(cpu_.memory_dump(), "init_memory_dump");
+	}
+
+	Emulator(const std::string& bootrom_path, const std::string& cartridge_path) {
+
+		const auto boot_rom = Cartridge{bootrom_path}.dump();
+		const auto rom = Cartridge{cartridge_path}.dump();
+
+		auto array = Memory::ArrayType{};
+
+		std::transform(begin(boot_rom), end(boot_rom), begin(array), [](const auto& el) { return static_cast<uint8_t>(el); });
+		std::transform(begin(rom) + 0x100, end(rom), begin(array) + 0x100, [](const auto& el) { return static_cast<uint8_t>(el); });
+
+		// TODO: Remove, for debug purposes only
+		array[0xff44] = 0x90;
+
+		const auto regs =
+			Registers{};
+
+		cpu_ = Cpu{array, regs};
+		raw_dump(cpu_.memory_dump(), "init_memory_dump");
+	}
+
+	auto execute_next() -> uint64_t {
+		return cpu_.execute_next();
+	}
+
+
+private:
+	Cpu cpu_ = Cpu{};
+
+};
--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,4 @@
-#include "cartridge.h"
-#include "cpu.h"
-#include "registers.h"
+#include "emulator.h"
 
 #include <array>
 #include <iostream>
@@ -19,34 +17,12 @@ auto main(int argc, const char** argv) -> int
 		return 1;
 	}
 
-	const auto boot = Cartridge{std::string(argv[1])};
-	const auto cart = Cartridge{std::string(argv[2])};
-	// cart.print_info();
-
-	const auto boot_rom = boot.dump();
-	const auto rom = cart.dump();
-
-	auto array = Memory::ArrayType{};
-
-	std::transform(begin(boot_rom), end(boot_rom), begin(array), [](const auto& el) { return static_cast<uint8_t>(el); });
-	std::transform(begin(rom) + 0x100, end(rom), begin(array) + 0x100, [](const auto& el) { return static_cast<uint8_t>(el); });
-
-	// TODO: Remove, for debug purposes only
-	array[0xff44] = 0x90;
+	auto emu = argc == 2 ? Emulator{argv[1]} : Emulator{argv[1], argv[2]};
 
 
-	// Initializing values same way bgb emualtor does it
-	// RegistersChanger{.AF = 0x01b0, .BC=0x0013, .DE = 0x00d8, .HL = 0x014d, .PC = 0x0100, .SP = 0xfffe}.get(Registers{});
-	const auto regs =
-		Registers{};
-
-	auto cpu = Cpu{array, regs};
-	raw_dump(cpu.memory_dump(), "init_memory_dump");
-
-
-	// auto counter = static_cast<uint64_t>(0);
+	auto counter = static_cast<uint64_t>(0);
 	while (1) {
-		[[maybe_unused]] const auto cycles = cpu.execute_next();
+		[[maybe_unused]] const auto cycles = emu.execute_next();
 		std::cout << "instructions executed: " << counter++ << '\n';
 	}
 	//

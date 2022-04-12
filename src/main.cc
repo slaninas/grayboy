748304cad09126f09dd24966b9186cf89d420928
slaninas <uglymuffin@protonmail.com>
2022-04-12 14:01:00 +0200

    Add comment

--- src/main.cc
+++ src/main.cc
@@ -10,6 +10,7 @@
 // ************************
 // - BootRom has no diff compared to https://github.com/wheremyfoodat/Gameboy-logs except last 100 lines but it should be just checksum (and I'm running different cartridge
 //   than the author of those dumps
+// - Blargg 03 works fine
 // - Blargg 04 works fine
 // - Blargg 05 works fine
 // - Blargg 06 works fine (PC must be set to 0x101 instead of 0x100 at the beginning)

33f2c5054eb34b4626c23445c83f9a213c0d239b
slaninas <uglymuffin@protonmail.com>
2022-04-16 22:03:52 +0200

    Wait just 10 ms

--- src/main.cc
+++ src/main.cc
@@ -53,7 +53,7 @@ auto main(int argc, const char** argv) -> int
 			if (!display.update(emu.get_memory())) {
 				return 0;
 			}
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
 
 		if (counter > 500'000'000) {

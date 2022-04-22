501923f151788d23f4e5452ac71e83b9ac47d190
slaninas <uglymuffin@protonmail.com>
2022-04-22 16:30:49 +0200

    Fix test after change in last commit (prevent ROM writes)

--- tests/memory_tests.cc
+++ tests/memory_tests.cc
@@ -49,54 +49,54 @@ TEST_CASE("Read/Write", "[memory]")
 	const auto orig_mem = getRandomMemory();
 	auto mem = orig_mem;
 
-	mem.write(0x00, 0xea);
-	CHECK(mem.read(0x00) == 0xea);
+	mem.write(0xc000, 0xea);
+	CHECK(mem.read(0xc000) == 0xea);
 
-	mem.write(0x01, 0xbc);
-	CHECK(mem.read(0x01) == 0xbc);
+	mem.write(0xc001, 0xbc);
+	CHECK(mem.read(0xc001) == 0xbc);
 
-	mem.write(0x02, 0x43);
-	CHECK(mem.read(0x02) == 0x43);
+	mem.write(0xc002, 0x43);
+	CHECK(mem.read(0xc002) == 0x43);
 
-	mem.write(0x03, 0xfe);
-	CHECK(mem.read(0x03) == 0xfe);
+	mem.write(0xc003, 0xfe);
+	CHECK(mem.read(0xc003) == 0xfe);
 
-	const auto correct_mem = MemoryChanger{{{0x00, 0xea}, {0x01, 0xbc}, {0x02, 0x43}, {0x03, 0xfe}}}.get(orig_mem);
+	const auto correct_mem = MemoryChanger{{{0xc000, 0xea}, {0xc001, 0xbc}, {0xc002, 0x43}, {0xc003, 0xfe}}}.get(orig_mem);
 	CHECK(mem.dump() == correct_mem.dump());
 }
 
 TEST_CASE("Memory snapshot", "[memory]")
 {
-	const auto orig_memory = MemoryChanger{{{0x34, 0x43}, {0xba, 0x12}, {0x01, 0x89}}}.get(getRandomMemory());
+	const auto orig_memory = MemoryChanger{{{0xc034, 0x43}, {0xc0ba, 0x12}, {0xc001, 0x89}}}.get(getRandomMemory());
 	auto snapshots = MemorySnapshots{orig_memory};
 	auto memory = orig_memory;
 
-	memory.write(0xba, 0x33);
+	memory.write(0xc0ba, 0x33);
 	snapshots.add(memory);
 
-	memory.write(0x34, 0x97);
+	memory.write(0xc034, 0x97);
 	snapshots.add(memory);
 
-	memory.write(0x01, 0xba);
+	memory.write(0xc001, 0xba);
 	snapshots.add(memory);
 
 	SECTION("Zero back")
 	{
 		memory = snapshots.getMemory(0);
-		const auto correct_memory = MemoryChanger{{{0xba, 0x33}, {0x34, 0x97}, {0x01, 0xba}}}.get(orig_memory);
+		const auto correct_memory = MemoryChanger{{{0xc0ba, 0x33}, {0xc034, 0x97}, {0xc001, 0xba}}}.get(orig_memory);
 		CHECK(memory.dump() == correct_memory.dump());
 	}
 
 	SECTION("One back")
 	{
 		memory = snapshots.getMemory(1);
-		const auto correct_memory = MemoryChanger{{{0xba, 0x33}, {0x34, 0x97}}}.get(orig_memory);
+		const auto correct_memory = MemoryChanger{{{0xc0ba, 0x33}, {0xc034, 0x97}}}.get(orig_memory);
 		CHECK(memory.dump() == correct_memory.dump());
 	}
 
 	SECTION("Two back")
 	{
-		const auto correct_memory = MemoryChanger{{{0xba, 0x33}}}.get(orig_memory);
+		const auto correct_memory = MemoryChanger{{{0xc0ba, 0x33}}}.get(orig_memory);
 		memory = snapshots.getMemory(2);
 		CHECK(memory.dump() == correct_memory.dump());
 	}
@@ -109,46 +109,46 @@ TEST_CASE("Memory snapshot", "[memory]")
 
 	SECTION("More writes at once")
 	{
-		memory.write(0x34, 0xcd);
-		memory.write(0xba, 0x02);
-		memory.write(0x01, 0x1b);
+		memory.write(0xc034, 0xcd);
+		memory.write(0xc0ba, 0x02);
+		memory.write(0xc001, 0x1b);
 		snapshots.add(memory);
 
-		const auto correct_memory = MemoryChanger{{{0xba, 0x02}, {0x34, 0xcd}, {0x01, 0x1b}}}.get(orig_memory);
+		const auto correct_memory = MemoryChanger{{{0xc0ba, 0x02}, {0xc034, 0xcd}, {0xc001, 0x1b}}}.get(orig_memory);
 		CHECK(snapshots.getMemory(0).dump() == correct_memory.dump());
 
 		// Should be same as "Zero back"
-		const auto correct_memory2 = MemoryChanger{{{0xba, 0x33}, {0x34, 0x97}, {0x01, 0xba}}}.get(orig_memory);
+		const auto correct_memory2 = MemoryChanger{{{0xc0ba, 0x33}, {0xc034, 0x97}, {0xc001, 0xba}}}.get(orig_memory);
 		CHECK(snapshots.getMemory(1).dump() == correct_memory2.dump());
 	}
 }
 
 TEST_CASE("Last diffs", "[memory]")
 {
-	const auto orig_memory = MemoryChanger{{{0x34, 0x43}, {0xba, 0x12}, {0x01, 0x89}}}.get(getRandomMemory());
+	const auto orig_memory = MemoryChanger{{{0xc034, 0x43}, {0xc0ba, 0x12}, {0xc001, 0x89}}}.get(getRandomMemory());
 	auto snapshots = MemorySnapshots{orig_memory};
 	auto memory = orig_memory;
 
-	memory.write(0xba, 0x33);
+	memory.write(0xc0ba, 0x33);
 	snapshots.add(memory);
 
-	memory.write(0x34, 0x97);
+	memory.write(0xc034, 0x97);
 	snapshots.add(memory);
 	snapshots.add(memory);
 
-	memory.write(0x01, 0xba);
-	memory.write(0xba, 0xcd);
+	memory.write(0xc001, 0xba);
+	memory.write(0xc0ba, 0xcd);
 	snapshots.add(memory);
 	snapshots.add(memory);
-	memory.write(0xba, 0x12);
+	memory.write(0xc0ba, 0x12);
 	snapshots.add(memory);
 
 	const auto last_diffs = snapshots.getLastNonEmptyDiffs(4);
 	const auto correct_diffs = std::vector<MemoryDiff>{
-		{0x34, 0x43, 0x97},
-		{0x01, 0x89, 0xba},
-		{0xba, 0x33, 0xcd},
-		{0xba, 0xcd, 0x12},
+		{0xc034, 0x43, 0x97},
+		{0xc001, 0x89, 0xba},
+		{0xc0ba, 0x33, 0xcd},
+		{0xc0ba, 0xcd, 0x12},
 	};
 	CHECK(last_diffs == correct_diffs);
 }

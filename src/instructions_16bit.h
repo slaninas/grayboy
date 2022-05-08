94b27318345823169cfdfd6784c20e0d9bda8102
slaninas <slaninas@pm.me>
2022-05-08 18:28:44 +0200

    Cleanup

--- src/instructions_16bit.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "instruction_utils.h"
-
-#include <vector>
-
-auto get_16bit_instructions() -> std::vector<Instruction>;
--- src/instructions_8bit.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "instruction_utils.h"
-
-auto get_8bit_instructions() -> std::vector<Instruction>;
